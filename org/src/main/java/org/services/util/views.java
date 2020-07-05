432d21f1bc69d57da4d658fda10f58a979a65e34
Oliver Sauren <55805960+OliverSauren@users.noreply.github.com>
2020-07-05 16:53:47 +0200

    Create views.java

---
+++ org/src/main/java/org/services/util/views.java
@@ -0,0 +1,9 @@
+package org.services.util;
+
+public class views {
+
+    public static final String MAIN = "main";
+    public static final String LOGIN = "login";
+    public static final String REGISTRIERUNG = "registrierung";
+
+}
